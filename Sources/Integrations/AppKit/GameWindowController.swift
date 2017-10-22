e7780634281c2a563616f89b97119d979728ea36
John Sundell <john@sundell.co>
2017-10-22 19:28:03 +0200

    GameWindowController: Require size in init and use it to create window
    Also make window non-resizable (for now), and make API match iOS.

--- Sources/Integrations/AppKit/GameWindowController.swift
+++ Sources/Integrations/AppKit/GameWindowController.swift
@@ -7,21 +7,31 @@
 
 import Cocoa
 
+/// Window controller that can be used to present & manage an Imagine Engine game
 public class GameWindowController: NSWindowController {
     /// The game that the window controller is managing
-    public var game: Game { return gameViewController.game }
-    
-    /// The game view controller that's presenting the game managed by the window controller
-    public var gameViewController: GameViewController { return contentViewController as! GameViewController }
-    
-    public convenience init(scene: Scene? = nil) {
-        let window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 640, height: 360), styleMask: [.closable, .miniaturizable, .resizable, .titled], backing: .buffered, defer: false)
+    public var game: Game { return viewController.game }
+    private let viewController: GameViewController
+
+    /// Initialize an instance of this window controller, optionally with a scene to present
+    public init(size: Size, scene: Scene? = nil) {
+        let scene = scene ?? Scene(size: size)
+        viewController = GameViewController(size: size, scene: scene)
+
+        let window = NSWindow(
+            contentRect: viewController.view.bounds,
+            styleMask: [.closable, .miniaturizable, .titled],
+            backing: .buffered,
+            defer: false
+        )
         
-        self.init(window: window)
-        
+        super.init(window: window)
+
+        contentViewController = viewController
         window.center()
-        
-        let effectiveScene = scene ?? Scene(size: window.contentRect(forFrameRect: window.frame).size)
-        contentViewController = GameViewController(scene: effectiveScene)
+    }
+
+    required public convenience init?(coder: NSCoder) {
+        self.init(size: .zero, scene: nil)
     }
 }
