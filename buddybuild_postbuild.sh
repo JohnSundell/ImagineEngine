99f4d2b3d822a07f20a071b67453da17df439458
John Sundell <john@sundell.co>
2017-11-12 13:06:58 +0100

    CI: Run Danger as part of build process instead
    Otherwise the PR status will become green before Danger has had a chance to run.

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Run tests on macOS + tvOS
 xcodebuild clean test -project ImagineEngine.xcodeproj -scheme ImagineEngine-macOS -destination "platform=OS X" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO
 xcodebuild clean test -project ImagineEngine.xcodeproj -scheme ImagineEngine-tvOS -destination "platform=tvOS Simulator,name=Apple TV 1080p" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO
+
+# Run danger
+bundle install
+bundle exec danger --fail-on-errors=true
