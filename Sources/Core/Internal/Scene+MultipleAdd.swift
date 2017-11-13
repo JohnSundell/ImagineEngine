42bcf5e09b083cd79f86756804f14770bbb812fa
João Pereira <knight4@gmail.com>
2017-11-13 10:01:18 +0000

    Fix lint

--- Sources/Core/Internal/Scene+MultipleAdd.swift
+++ Sources/Core/Internal/Scene+MultipleAdd.swift
@@ -7,15 +7,15 @@
 import Foundation
 
 extension Scene {
-    
+
     public func add(_ actors: Actor...) {
         actors.forEach(add)
     }
-    
+
     public func add(_ labels: Label...) {
         labels.forEach(add)
     }
-    
+
     public func add(_ blocks: Block...) {
         blocks.forEach(add)
     }
