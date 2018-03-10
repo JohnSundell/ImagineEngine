890b7d15c003d1ac228b1f1b03a183c7aa32b44b
John Sundell <john@sundell.co>
2018-03-10 23:19:54 +0100

    SpriteSheetTests: Use macOS-specific assertions
    Since macOS uses a flipped coordinate system compared to iOS/tvOS, we need
    to assert the content rect when creating sprite sheet frames according to
    the correct coordinate system.

--- Tests/ImagineEngineTests/SpriteSheetTests.swift
+++ Tests/ImagineEngineTests/SpriteSheetTests.swift
@@ -12,9 +12,9 @@ final class SpriteSheetTests: XCTestCase {
         let sheet = SpriteSheet(textureNamed: "Sheet", frameCount: 5, rowCount: 1)
 
         let frame1 = sheet.frame(at: 0)
-        XCTAssertEqual(frame1.contentRect, Rect(x: 0, y: 0, width: 0.2, height: 1))
-
         let frame4 = sheet.frame(at: 3)
+
+        XCTAssertEqual(frame1.contentRect, Rect(x: 0, y: 0, width: 0.2, height: 1))
         XCTAssertEqual(frame4.contentRect, Rect(x: 0.6, y: 0, width: 0.2, height: 1))
     }
 
@@ -22,13 +22,18 @@ final class SpriteSheetTests: XCTestCase {
         let sheet = SpriteSheet(textureNamed: "Sheet", frameCount: 8, rowCount: 2)
 
         let frame1 = sheet.frame(at: 0)
-        XCTAssertEqual(frame1.contentRect, Rect(x: 0, y: 0, width: 0.25, height: 0.5))
-
         let frame4 = sheet.frame(at: 3)
-        XCTAssertEqual(frame4.contentRect, Rect(x: 0.75, y: 0, width: 0.25, height: 0.5))
-
         let frame7 = sheet.frame(at: 6)
+
+        #if os(macOS)
+        XCTAssertEqual(frame1.contentRect, Rect(x: 0, y: 0.5, width: 0.25, height: 0.5))
+        XCTAssertEqual(frame4.contentRect, Rect(x: 0.75, y: 0.5, width: 0.25, height: 0.5))
+        XCTAssertEqual(frame7.contentRect, Rect(x: 0.5, y: 0, width: 0.25, height: 0.5))
+        #else
+        XCTAssertEqual(frame1.contentRect, Rect(x: 0, y: 0, width: 0.25, height: 0.5))
+        XCTAssertEqual(frame4.contentRect, Rect(x: 0.75, y: 0, width: 0.25, height: 0.5))
         XCTAssertEqual(frame7.contentRect, Rect(x: 0.5, y: 0.5, width: 0.25, height: 0.5))
+        #endif
     }
 
     func testGeneratingFramesFromSlicedSpriteSheet() {
@@ -36,9 +41,14 @@ final class SpriteSheetTests: XCTestCase {
         let slice = sheet.slice(from: Coordinate(x: 2, y: 1)...Coordinate(x: 3, y: 2))
 
         let frame1 = slice.frame(at: 0)
-        XCTAssertEqual(frame1.contentRect, Rect(x: 0.5, y: 0.25, width: 0.25, height: 0.25))
-
         let frame4 = slice.frame(at: 3)
+
+        #if os(macOS)
+        XCTAssertEqual(frame1.contentRect, Rect(x: 0.5, y: 0.5, width: 0.25, height: 0.25))
+        XCTAssertEqual(frame4.contentRect, Rect(x: 0.75, y: 0.25, width: 0.25, height: 0.25))
+        #else
+        XCTAssertEqual(frame1.contentRect, Rect(x: 0.5, y: 0.25, width: 0.25, height: 0.25))
         XCTAssertEqual(frame4.contentRect, Rect(x: 0.75, y: 0.5, width: 0.25, height: 0.25))
+        #endif
     }
 }
